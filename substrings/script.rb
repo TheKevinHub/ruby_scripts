7cac32e19985a56144460bcdd15cd00fc1ce4190
TheKevinHub <kevin.accetta95@gmail.com>
2023-12-11 17:08:10 -0400

    Finish substrings method script

---
+++ substrings/script.rb
@@ -0,0 +1,44 @@
+# Implement #substrings method that takes a word and an array of valid substrings (dictionary)
+# as arguments. Return a hash listing each substring (case insensitive) that was found
+# in the original string and how many times it was found.
+
+# Make sure it can handle multiple words
+
+# TIPS
+# .split (string to array, divided by words)
+# .join(' ') (array to string with space in between words)
+# .count (count elements in array)
+
+dictionary = [
+    "below", "go", "down", "above", "how", "hello", "up",
+    "it", "return", "us", "world", "i", "low", "high",
+    "where", "test", "testing", "part", "dictionary",
+    "forever", "program", "done", "sit", "stand", "play",
+    "stop", "average", "going", "with", "are", "goodbye",
+    "hey", "you", "should", "out", "soon", "doing"
+]
+
+def substrings(string, dictionary)
+    # gsub! to remove special characters
+    stringArray = string.gsub!(/[^0-9A-Za-z]/, ' ').downcase.split
+    
+    duplicates = []
+    hash = {}
+
+    stringArray.each do |element|
+        isInDict = dictionary.include?(element)
+
+        if isInDict
+            if hash.key?(element)
+                val = hash[element] + 1
+                hash[element] = val
+            else
+                hash[element] = 1
+            end
+        end
+    end
+
+    p hash
+end
+
+substrings("Hey, how are you doing? Are we doing stuff later?", dictionary)
